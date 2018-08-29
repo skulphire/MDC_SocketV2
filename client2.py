89815eb4d3a33ee5cf57876352fc90233ed73f54
ckinney <cdkrobotics@gmail.com>
2018-08-28 23:59:36 -0400

    troubleshoot

--- client2.py
+++ client2.py
@@ -3,7 +3,7 @@ import socket
 from SocketScripts import SocketHelper as helper
 
 host = "100.16.100.180"
-port = 9130
+port = 20000 #9130
 BUFFER_SIZE = 2000
 MESSAGE = input("tcpClientB: Enter message/ Enter exit:")
 
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from SocketScripts import SocketConnect2
 if __name__ == '__main__':
     try:
-        socket = SocketConnect2.ThreadedServer()
+        socket = SocketConnect2.ThreadedServer(serverPort=20000)
         socket.startServer()
     except KeyboardInterrupt:
         exit(0)
