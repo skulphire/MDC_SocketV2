e6a73ffd7357f1766f86170e0cca91cc47f37596
ckinney <cdkrobotics@gmail.com>
2018-08-31 01:57:23 -0400

    Testing new code

--- FileScripts/userfiles.py
+++ FileScripts/userfiles.py
@@ -3,8 +3,9 @@ import os
 def checkIfSuper(user,org="ADPS"):
     workingdir = os.getcwd()
     filedir = workingdir + "/MDC-Files/" + org+"-Users/"
-    print("File Dir: "+filedir)
-    if(user in filedir):
+    filedirlist = os.listdir(filedir)
+    #print("File Dir: "+filedir)
+    if(user in filedirlist):
         with open(user) as f:
             lines = f.readlines()
         if("Super" in lines[1]):
