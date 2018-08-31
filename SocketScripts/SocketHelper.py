4a77b0765aa10137dd4db534bd25b9f6c9cf2df6
ckinney <cdkrobotics@gmail.com>
2018-08-31 12:37:37 -0400

    Testing new code

--- SocketScripts/SocketHelper.py
+++ SocketScripts/SocketHelper.py
@@ -22,10 +22,14 @@ def initValidUsers(org = "ADPS"):
     workingDir = os.getcwd()
     dir = workingDir + "/MDC-Files/" + userDir
     try:
-        globals.VALIDUSERS = os.listdir(dir)
-        for user in globals.VALIDUSERS:
-            print("User>"+user)
-            globals.AREUSERSLOGGEDIN[user] = False
+        change = os.listdir(dir)
+        for user in change:
+            splits = os.path.splitext(dir+user)
+            globals.VALIDUSERS[splits[0]] = False
+
+        #for user in globals.VALIDUSERS:
+        #    print("User>"+user)
+        #    globals.AREUSERSLOGGEDIN[user] = False
     except Exception:
         print("No Files Found in: "+dir)
 
