33152c4d45c098821cee80ebd4bbe920c7846389
ckinney <cdkrobotics@gmail.com>
2018-08-31 02:40:45 -0400

    Testing new code

--- FileScripts/userfiles.py
+++ FileScripts/userfiles.py
@@ -6,16 +6,17 @@ def checkIfSuper(user,org="ADPS"):
     print("File Dir: " + filedir)
     try:
         filedirlist = os.listdir(filedir)
-        return True
+
+        if (user in filedirlist):
+            with open(user) as f:
+                lines = f.readlines()
+            if ("Super" in lines[1]):
+                return True
+        else:
+            print("User does not exist")
+            return False
     except Exception as e:
         print(e)
         return False
-    # if(user in filedirlist):
-    #     with open(user) as f:
-    #         lines = f.readlines()
-    #     if("Super" in lines[1]):
-    #         return True
-    # else:
-    #     print("User does not exist")
-    #     return False
+
 #def checkGamerTag(user,org="ADPS"):
