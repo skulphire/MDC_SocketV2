32a102dc374ed6eda7d8e6024d3a538fdca899e9
ckinney <cdkrobotics@gmail.com>
2018-09-01 13:24:47 -0400

    Testing new code

--- FileScripts/senariosconversations.py
+++ FileScripts/senariosconversations.py
@@ -4,11 +4,15 @@ import random
 workingdir = os.getcwd() + "/MDC-Files/Senarios/Conversations/"
 
 def readScript(script):
-    path = os.path.realpath(workingdir+script+".txt")
-    with open(path) as f:
-        count = 0
-        for line in f:
-            count=count+1
-        rnd = random.randint(0,count)
-        lines = f.readlines()
-    return lines[rnd]
+    try:
+        path = os.path.realpath(workingdir+script+".txt")
+        with open(path) as f:
+            count = 0
+            for line in f:
+                count=count+1
+            rnd = random.randint(0,count)
+            lines = f.readlines()
+        return lines[rnd]
+    except Exception as e:
+        print(e)
+        return "Exception"
