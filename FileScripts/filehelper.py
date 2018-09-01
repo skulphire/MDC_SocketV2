dad4152bedd7eec7ee6ae19f7139c73317e02060
ckinney <cdkrobotics@gmail.com>
2018-09-01 19:11:03 -0400

    Testing new code

--- FileScripts/filehelper.py
+++ FileScripts/filehelper.py
@@ -3,12 +3,16 @@ import os
 workingdir = os.getcwd() + "/MDC-Files/"
 
 def checkdirectory(directorytocheck, org="ADPS"):
-    path = os.path.realpath(workingdir+org+"-"+directorytocheck)+"/"
-    contents = os.listdir(path)
-    newcontents = []
-    for entry in contents:
-        entry = entry.replace('[',"")
-        entry = entry.replace(']', "")
-        entry = entry.replace("'", "")
-        newcontents.append(entry)
-    return newcontents
+    try:
+        path = os.path.realpath(workingdir+org+"-"+directorytocheck)+"/"
+        contents = os.listdir(path)
+        newcontents = []
+        for entry in contents:
+            entry = entry.replace('[',"")
+            entry = entry.replace(']', "")
+            entry = entry.replace("'", "")
+            newcontents.append(entry)
+        return newcontents
+    except Exception as e:
+        print(e)
+        return "none"
