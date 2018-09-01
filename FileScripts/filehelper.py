0c5bbb9f3cee8e12d2519af63e71dfef7c9c9bb0
ckinney <cdkrobotics@gmail.com>
2018-09-01 19:43:42 -0400

    Testing new code

--- FileScripts/filehelper.py
+++ FileScripts/filehelper.py
@@ -8,10 +8,7 @@ def checkdirectory(directorytocheck, org="ADPS"):
         contents = os.listdir(path)
         newcontents = ""
         for entry in contents:
-            print("Dir contents: "+entry)
-            # entry = entry.replace(']', "")
-            # entry = entry.replace("'", "")
-            # entry = entry.replace('[',"")
+            #print("Dir contents: "+entry)
             splits = entry.split(".")
             newcontents = newcontents +splits[0] +","
         return newcontents
