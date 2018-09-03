6dfb911470ba0dcc12eeb83eb55343827df1d22d
ckinney <cdkrobotics@gmail.com>
2018-09-03 01:13:05 -0400

    Testing new code

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 # Python TCP Client B
 import socket
 from SocketScripts import SocketHelper as helper
+import os
 
 host = "opsidiumdesigns.com"
 port = 9130
@@ -15,6 +16,17 @@ try:
         data = tcpClientB.recv(BUFFER_SIZE)
         print(" Client received data:", data)
         MESSAGE = input("tcpClientB: Enter message to continue/ Enter exit:")
+        if("json" in MESSAGE):
+            file = os.path.realpath(os.getcwd()+"TempObjects/")+"senarioData_21146.json"
+            with open(file) as f:
+                lines = f.readlines()
+            tcpClientB.send(helper.convertToBytes("#senario"))
+            d = tcpClientB.recv(BUFFER_SIZE)
+            print(helper.convertToString(d))
+            for line in lines:
+                tcpClientB.send((helper.convertToBytes(line)))
+                print(line)
+            tcpClientB.send(helper.convertToBytes("#end-senario"))
     tcpClientB.close()
 except:
     tcpClientB.close()
