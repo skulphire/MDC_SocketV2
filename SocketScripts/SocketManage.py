769e3ed0aa92a54138ad1fbf775e8fa7ab3f1c79
aphire <cdkrobotics@gmail.com>
2018-09-06 02:22:53 -0400

    testing

--- SocketScripts/SocketManage.py
+++ SocketScripts/SocketManage.py
@@ -111,7 +111,7 @@ class SocketManage(object):
                     client.send(helper.convertToBytes("ack"))
                     splits = data.split(":")
                     buffer = int(splits[1])
-                    print(buffer*2)
+                    print(buffer*6)
                     data2 = " "
                     try:
                         data2 = client.recv(buffer)
