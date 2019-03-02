349b82c2a77c5f66e4944155d4289f0afb7bc1c6
ckinney <cdkrobotics@gmail.com>
2019-03-02 17:25:48 -0500

    added local client

---
+++ LOCALclient.py
@@ -0,0 +1,37 @@
+# Python TCP Client B
+import socket
+from SocketScripts import SocketHelper as helper
+import os
+import time
+
+host = "10.10.1.132"
+port = 9130
+BUFFER_SIZE = 2048
+MESSAGE = input("tcpClientB: Enter message/ Enter exit:")
+
+tcpClientB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpClientB.connect((host, port))
+try:
+    while MESSAGE != 'exit':
+        tcpClientB.send(helper.convertToBytes(MESSAGE))
+        data = tcpClientB.recv(BUFFER_SIZE)
+        print(" Client received data:", data)
+        MESSAGE = input("tcpClientB: Enter message to continue/ Enter exit:")
+        if("json" in MESSAGE):
+            file = os.path.realpath(os.getcwd()+"/TempObjects/")+"/senarioData_21146.json"
+            print(file)
+            with open(file) as f:
+                lines = f.readlines()
+            tcpClientB.send(helper.convertToBytes("#senario"))
+            print("#senario")
+            d = tcpClientB.recv(BUFFER_SIZE)
+            print(helper.convertToString(d))
+            for line in lines:
+                tcpClientB.send((helper.convertToBytes(line)))
+                time.sleep(.02)
+                #print(line)
+            tcpClientB.send(helper.convertToBytes("#end-senario"))
+            print("#end-senario")
+    tcpClientB.close()
+except:
+    tcpClientB.close()
